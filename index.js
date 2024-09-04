a080d44f3464fd9fb491a50f77b75e8f8c0372af
Zlindsay26 <zlindsay260602@gmail.com>
2024-09-03 23:08:38 -0400

    make html form, add new js functions
    form added to html markup; 'addbook' and 'displaybok'
    functions added

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const myLibrary = [];
+const myLibrary = [
+    {title: "To Kill A Mockingbird", author: "Harper Lee", pages: "281", read: "true"},
+    {title: "1984", author: "George Orwell", pages: "328", read: "false"},
+    {title: "Moby Dick", author: "Herman Melville", pages: "635", read: "true"},
+];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = Number(document.getElementById('pages').value);
+    const read = document.querySelector('input[name="read"]:checked').value === 'true';
+    const newBook = new Book(title, author, pages, read);
+    if (!title || !author || isNaN(pages) || pages <= 0 || !read) {
+        alert('Please fill in all fields correctly.');
+        return;
+    }
+    
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    document.getElementById('book-form').reset();
 }
+
+function displayBooks(library) {
+library.forEach(function(book) {
+    console.log("Title: " + book.title);
+    console.log("Author: " + book.author);
+    console.log("Pages: " + book.pages);
+    console.log("Read: " + book.read);
+    console.log("----------");
+})
+};
+displayBooks(myLibrary);
+    
